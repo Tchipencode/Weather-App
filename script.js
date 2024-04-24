12fa6505e8d5414177d51f79cfc9c0e797bf7a50
Tchipen <153777126+Tchipencode@users.noreply.github.com>
2024-04-24 13:49:44 +0100

    app done

--- script.js
+++ script.js
@@ -1,92 +1,99 @@
-const search=document.querySelector("#search-city");
-const btn=document.querySelector("#submit");
+const main= document.querySelector(".main-content");
+const body=document.querySelector("body");
+const search=document.querySelector("#location");
+const btnSubmit=document.querySelector("#submit");
 const form=document.querySelector("#form")
-const cityValue=document.querySelector(".city+span");
-const countryValue=document.querySelector(".country+span");
-const conditionValue=document.querySelector(".condition+span");
-const tempValue=document.querySelector(".temp+span");
-const humValue=document.querySelector(".hum+span");
-const windValue=document.querySelector(".wind+span");
+const cityValue=document.querySelector(".city>span");
+const countryValue=document.querySelector(".country>span");
+const conditionValue=document.querySelector(".condition>span");
+const icon=document.querySelector("#icon");
+const tempValue=document.querySelector(".temp>span");
+const humValue=document.querySelector(".hum>span");
+const windValue=document.querySelector(".wind>span");
 const btnToggle=document.querySelector("#toggle");
-
-class Weather {
-   constructor(cityName, country, condition, tempC, tempF, hum, wind) {
-      this.cityName=cityName,
-      this.country=country,
-      this.condition=condition,
-      this.tempC=tempC,
-      this.tempF=tempF,
-      this.hum=hum,
-      this.wind=wind;
-         
-   }
-}
+const weatherContent=document.querySelector(".weather-content");
+const loader=document.querySelector(".loader");
+const result=document.querySelector(".result");
+let getData=null;
+let currentTemp="";
+let tempUnit="";
 
 async function getWeather(city){
    try {
       const response= await fetch(`http://api.weatherapi.com/v1/current.json?key=20a1fc707b85408583a133738242204&q=${city}`, {mode :'cors'});
       if(!response.ok){
-         throw new Error("Network response has issue");
+         throw new Error("Network response has issue:",response.status);
       }
-      const getData= await response.json();
-      // const cityName= getData.location.name;
-      // const country= getData.location.country;
-      // const condition=getData.current.condition.text;
-      // const tempC=getData.current.temp_c;
-      // const tempF=getData.current.temp_f;
-      // const hum=getData.current.humidity;
-      // const wind=getData.current.wind_kph;
-      
-      // weatherObject(cityName, country, condition, tempC,tempF, hum, wind);
+      getData= await response.json();
+
       showWeather(getData);
       console.log(getData);
       
    } catch (error) {
-      if(error==200){
-         console.error("there is a problem with your fetch operation:",error);
-      }
+      console.error("there is a problem with your fetch operation:",error);
       
    }
 }
-// getWeather();
-// btn.addEventListener("click", ()=>{
+async function getWeatherIcon(getData){
+   try{
+      const iconText=getData.current.condition.text;
+      const response= await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=RNFYTt4qXKZHJvdgmUa1EVXYa7NXcLCs&s=${iconText}&g=g`, {mode: 'cors'});
+      if(!response.ok){
+         throw new Error("Network response has issue:",response.status);
+      }
+      const dataImg= await response.json();
+      // const titleIcon=dataImg.filter(gif=>gif.data.title.includes(`${iconText}`));
+      body.style.backgroundImage=`url(${dataImg.data.images.original.url})`;
+      console.log(response, dataImg,body.style.backgroundImage);
+      
+
+   } catch (error) {
+      console.error("there is a problem with your fetch operation:",error);
+      
+   }
    
-// });
-form.addEventListener("submit", (e)=>{
+}
+
+btnSubmit.addEventListener("click", async()=>{
    const city=search.value;
-   getWeather(city);
+   result.style.display="none";
+   loader.textContent="Loading...";
+   await getWeather(city);
+   await getWeatherIcon(getData)
+   loader.style.display="none";
+   result.style.display="block";
+
+   console.log(loader.textContent);
    
-   console.log(city);
+});
+form.addEventListener("submit", (e)=>{
    e.preventDefault();
+  
 })
-// function weatherObject(cityName, country, condition, tempC, tempF, hum, wind){
-//    const weather=new Weather(cityName, country, condition, tempC, tempF, hum, wind);
-//    console.log(weather);
-// }
-let isCelsius=true;
-let currentTemp=null;
-let tempUnit=null;
+
 function showWeather(getData){
    tempUnit='°C';
    currentTemp=getData.current.temp_c;
    cityValue.textContent=getData.location.name;
    countryValue.textContent=getData.location.country;
    conditionValue.textContent=getData.current.condition.text;
+   icon.src=`https:${getData.current.condition.icon}`;
    tempValue.textContent= currentTemp+tempUnit;
    humValue.textContent=`${getData.current.humidity}%`;
-   windValue.textContent=`${getData.current.wind_kph}kph`;
-   btnToggle.addEventListener("click",()=>{
-      cfSwitch(getData);
-   });
-   
+   windValue.textContent=`${getData.current.wind_kph}kph`;  
 }
-function cfSwitch(currentTemp, getData){
+
+btnToggle.addEventListener("click",cfSwitch);
+
+function cfSwitch(){
    if(currentTemp===getData.current.temp_c){
-      currentTemp=getData.current.temp_c;
+      currentTemp=getData.current.temp_f;
       tempUnit='°F';
-   }else{
+      tempValue.textContent= currentTemp+tempUnit;
+   }
+   else{
       currentTemp=getData.current.temp_c;
       tempUnit='°C';
+      tempValue.textContent= currentTemp+tempUnit;
    }
-
 }
